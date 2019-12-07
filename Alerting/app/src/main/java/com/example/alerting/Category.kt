fc6fd0d18472218ba0d7918673184b709f6c6f6e
ushuaibunuhu <smardvoi01@gmail.com>
2019-12-07 17:59:43 +0100

    Add files via upload

---
+++ Alerting/app/src/main/java/com/example/alerting/Category.kt
@@ -0,0 +1,12 @@
+package com.example.alerting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Category : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
---
+++ Alerting/app/src/main/java/com/example/alerting/Emergency.kt
@@ -0,0 +1,12 @@
+package com.example.alerting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Emergency : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emergency)
+    }
+}
